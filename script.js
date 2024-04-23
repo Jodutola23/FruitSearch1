0532fe0a49748ecfa36af2ba6094e0f4da96372f
Jodutola23 <155337470+Jodutola23@users.noreply.github.com>
2024-04-22 20:42:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Select the input field and suggestions list element from the DOM
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
+// Array of fruit names
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+// Function to search for fruits that include the search string
 function search(str) {
 	let results = [];
 	if (str.length){
@@ -13,13 +16,13 @@ function search(str) {
 
 	return results;
 }
-
+// Handler for search input's keyup event
 function searchHandler(e) {
 	const inputVal = e.target.value;
   const results = search(inputVal);
   showSuggestions(results, inputVal);
 }
-
+// Function to display suggestions based on search results
 function showSuggestions(results, inputVal) {
 	suggestions.innerHTML = ''; // Clear current suggestions
 	if (results.length) {
@@ -35,7 +38,7 @@ function showSuggestions(results, inputVal) {
 	  suggestions.style.display = 'none'; // Hide the suggestions if no results
 	}
 }
-
+// Function to handle click events on suggestions
 function useSuggestion(e) {
 	if (e.target.tagName === 'LI') {
 		const selectedValue = e.target.textContent;
@@ -44,13 +47,13 @@ function useSuggestion(e) {
 	  }
 	
 }
-
+// Function to hide suggestions with a delay, allowing for click events to be processed
 function hideSuggestions() {
 	setTimeout(() => { // Use setTimeout to delay the hiding, allowing click events to be processed
 	  suggestions.style.display = 'none';
 	}, 200); // Delay hiding to allow selection of an item
   }
-  
+// Event listeners for handling input focus, blur, and keyup events, and clicks on suggestions  
   input.addEventListener('blur', hideSuggestions);
   input.addEventListener('focus', () => suggestions.style.display = 'block');
 
